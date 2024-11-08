f0425f6b30f65ce79b1d62ecc5be4cf3df6655ee
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-08 05:02:46 +0300

    change

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 function Item({ name, category }) {
-  // State to track if the item is in the cart
+ 
   const [isInCart, setIsInCart] = useState(false);
 
-  // Toggle function to add/remove item from cart
+
   const handleCartToggle = () => {
     setIsInCart((prevIsInCart) => !prevIsInCart);
   };
